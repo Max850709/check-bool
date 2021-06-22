5faf29379e5ba82848802bb2a6b3e7266d1774d0
楊詔欽 <Max@yangzhaoqinde-MacBook-Air.local>
2021-06-22 13:57:51 +0800

    Initial Commit

---
+++ checkbool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  checkbool
+//
+//  Created by Max on 2021/6/22.
+//  Copyright © 2021年 Max. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
