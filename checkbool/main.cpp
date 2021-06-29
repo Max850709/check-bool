358e2e65082414b47ad52ff8b95a1662746d4da7
max850709 <amax0118amax@gmail.com>
2021-06-29 16:01:15 +0800

    main cpp

--- checkbool/main.cpp
+++ checkbool/main.cpp
@@ -7,9 +7,81 @@
 //
 
 #include <iostream>
+#include <stack>
+#include <cstring>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    int n;
+    string s;
+    cin >> n;
+    cin.get();
+    for(int i=0;i<n;i++){
+        getline(cin,s);
+        int boolean=0;
+        stack <int> myStack;
+        for(int j=0;j<s.size();j++){
+            if(s.at(j)=='[' || s.at(j)=='{' || s.at(j)=='<' || s.at(j)=='('){
+                myStack.push(s.at(j));
+            }
+            else if(s.at(j)==']' || s.at(j)=='}' || s.at(j)=='>' || s.at(j)==')'){
+                if (myStack.size() <=0){
+                    boolean+=1;
+                }
+                else if(s.at(j)==']'){
+                    char pair = myStack.top();
+                    if (pair =='['){
+                        myStack.pop();
+                    }
+                    else{
+                        boolean+=1;
+                    }
+                }
+                else if(s.at(j)=='}'){
+                    
+                    char pair = myStack.top();
+                    if (pair =='{'){
+                        myStack.pop();
+                    }
+                    else{
+                        boolean+=1;
+                    }
+                }
+                else if(s.at(j)=='>'){
+                    
+                    char pair = myStack.top();
+                    if (pair =='<'){
+                        myStack.pop();
+                    }
+                    else{
+                        boolean+=1;
+                    }
+                }
+                else if(s.at(j)==')'){
+                    
+                    char pair = myStack.top();
+                    if (pair =='('){
+                        myStack.pop();
+                    }
+                    else{
+                        boolean+=1;
+                    }
+                }
+            }
+        }
+        if(myStack.size()>0){
+            boolean+=1;
+        }
+        
+        if(boolean==0){
+            cout << "Y" << endl;
+        }
+        else{
+            cout << "N" << endl;
+        }
+    }
     return 0;
 }
